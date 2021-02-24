5070fd86826874165979b69c0dd1fab664f03b16
hunglt4 <hunglt@Aladintech.co>
2021-02-24 20:24:20 +0700

    '1st'

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,27 +15,25 @@ class Header extends Component {
                             <li class="nav-item active">
                                 <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Link</a>
-                            </li>
-                            <li class="nav-item dropdown">
+                            <li class="nav-item dropdown navbar-light active">
                                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    Dropdown
-        </a>
+                                    Recipes
+                                </a>
                                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="#">Action</a>
-                                    <a class="dropdown-item" href="#">Another action</a>
+                                    <a class="dropdown-item" href="#">Healthy Recipes</a>
                                     <div class="dropdown-divider"></div>
-                                    <a class="dropdown-item" href="#">Something else here</a>
+                                    <a class="dropdown-item" href="#">Easy Recipes</a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="#">Daily Days</a>
                                 </div>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link disabled" href="#">Disabled</a>
+                            <li class="nav-item active">
+                                <a class="nav-link disabled" href="#">About Us</a>
                             </li>
                         </ul>
                         <form class="form-inline my-2 my-lg-0">
-                            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-                            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                            <input class="form-control mr-sm-2" type="search" placeholder="Search Cookies..." aria-label="Search" icon="fa fa-icon-search" />
+                            <button class="btn btn-warning my-2 my-sm-0" type="submit">Login</button>
                         </form>
                     </div>
                 </nav>
