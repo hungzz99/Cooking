8ed875d45e81034e1f1071b9da7fe5cdb8e0bec6
Le Hung <hunglt@aladintech.co>
2021-02-24 17:48:48 +0700

    1st

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,34 +6,37 @@ class Header extends Component {
         return (
             <div className="header">
                 <nav className="navbar navbar-expand navbar-light bg-color">
+
                     <a className="navbar-brand" href="#">
                         <img src={logo} className="logo" />
                     </a>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav mr-auto">
-                            <form className="form-inline">
-                                <input className="form-control mr-sm-2" type="search" placeholder="Search" />
-                            </form>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Link</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Dropdown
+        </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="#">Action</a>
+                                    <a class="dropdown-item" href="#">Another action</a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="#">Something else here</a>
+                                </div>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link disabled" href="#">Disabled</a>
+                            </li>
                         </ul>
-                        <ul className="nav navbar-nav flex-fill justify-content-center">
-                            <li className="nav-item active">
-                                <a className="nav-link" href="#Home">Home</a>
-                            </li>
-                            <li className="nav-item active ">
-                                <a className="nav-link" href="#Download">Download</a>
-                            </li>
-                            <li className="nav-item active">
-                                <a className="nav-link " href="#Register">Register</a>
-                            </li>
-                            <li className="nav-item active">
-                                <a className="nav-link" href="#More">More</a>
-                            </li>
-                        </ul>
-                        <nav className="navbar-nav flex-row ml-md-auto d-none d-md-flex"></nav>
-                        <form className="form-inline">
-                            <input className="btn btn-success" type="submit" value="Login"/>
+                        <form class="form-inline my-2 my-lg-0">
+                            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
                         </form>
-
                     </div>
                 </nav>
             </div>
