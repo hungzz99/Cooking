5a535a2b546362ef45b4bcfa5703c5a5c4e50d69
Le Hung <hunglt@aladintech.co>
2021-03-03 08:26:30 +0700

    'main'

--- src/components/Main.js
+++ src/components/Main.js
@@ -25,15 +25,101 @@ class Main extends Component {
                         </div>
                         <img className="mr-3" src={maintiitle} alt="Generic placeholder image" />
                     </div>
-                    <div className="media-tittle">
+                    <br />
+                    <div className="media-title" flex-direction="row">
                         <h3> All Recipes</h3>
-                        <div className="card">
-                            <img className="card-img-top" src={chicken} alt="Generic placeholder image" />
-                            <div className="card-body">
-                                <h5 className="card-title" align="center">Chicken Dry</h5>
+                        <div>
+                            <div className="card">
+                                <div className="row">
+                                    <a className="col">
+                                        <img className="card-img-top" src={chicken} alt="Generic placeholder image" />
+                                        <div className="card-body">
+                                            <h5 className="card-title" align="center">Chicken Dry</h5>
+                                        </div>
+                                    </a>
+                                    <a className="col">
+                                        <img className="card-img-top" src={chicken} alt="Generic placeholder image" />
+                                        <div className="card-body">
+                                            <h5 className="card-title" align="center">Chicken Dry</h5>
+                                        </div>
+                                    </a>
+                                    <a className="col">
+                                        <img className="card-img-top" src={chicken} alt="Generic placeholder image" />
+                                        <div className="card-body">
+                                            <h5 className="card-title" align="center">Chicken Dry</h5>
+                                        </div>
+                                    </a>
+                                    <a className="col">
+                                        <img className="card-img-top" src={chicken} alt="Generic placeholder image" />
+                                        <div className="card-body">
+                                            <h5 className="card-title" align="center">Chicken Dry</h5>
+                                        </div>
+                                    </a>
+                                    {/* <div className="container">
+                                        <img src={chicken}  className="image" />
+                                        <div className="overlay">My Name is John</div>
+                                    </div>
+                                    <div className="container">
+                                        <img src={chicken}  className="image" />
+                                        <div className="overlay">My Name is John</div>
+                                    </div>
+                                    <div className="container">
+                                        <img src={chicken}  className="image" />
+                                        <div className="overlay">My Name is John</div>
+                                    </div>
+                                    <div className="container">
+                                        <img src={chicken}  className="image" />
+                                        <div className="overlay">My Name is John</div>
+                                    </div> */}
+                                </div>
+                            </div>
+                            <div className="card">
+                                <div className="row">
+                                    <a className="col">
+                                        <img className="card-img-top" src={chicken} alt="Generic placeholder image" />
+                                        <div className="card-body">
+                                            <h5 className="card-title" align="center">Chicken Dry</h5>
+                                        </div>
+                                    </a>
+                                    <a className="col">
+                                        <img className="card-img-top" src={chicken} alt="Generic placeholder image" />
+                                        <div className="card-body">
+                                            <h5 className="card-title" align="center">Chicken Dry</h5>
+                                        </div>
+                                    </a>
+                                    <a className="col">
+                                        <img className="card-img-top" src={chicken} alt="Generic placeholder image" />
+                                        <div className="card-body">
+                                            <h5 className="card-title" align="center">Chicken Dry</h5>
+                                        </div>
+                                    </a>
+                                    <a className="col">
+                                        <img className="card-img-top" src={chicken} alt="Generic placeholder image" />
+                                        <div className="card-body">
+                                            <h5 className="card-title" align="center">Chicken Dry</h5>
+                                        </div>
+                                    </a>
+                                    {/* <div className="container">
+                                        <img src={chicken}  className="image" />
+                                        <div className="overlay">My Name is John</div>
+                                    </div>
+                                    <div className="container">
+                                        <img src={chicken}  className="image" />
+                                        <div className="overlay">My Name is John</div>
+                                    </div>
+                                    <div className="container">
+                                        <img src={chicken}  className="image" />
+                                        <div className="overlay">My Name is John</div>
+                                    </div>
+                                    <div className="container">
+                                        <img src={chicken}  className="image" />
+                                        <div className="overlay">My Name is John</div>
+                                    </div> */}
+                                </div>
                             </div>
                         </div>
                     </div>
+
                 </div>
 
             </div>
