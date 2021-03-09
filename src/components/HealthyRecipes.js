12c4e575da6e0aa3cf87873e79f82b216ac9522c
Le Hung <hunglt@aladintech.co>
2021-03-09 16:08:07 +0700

    'healthyrecipes'

--- src/components/HealthyRecipes.js
+++ src/components/HealthyRecipes.js
@@ -1,12 +1,81 @@
 import React, { Component } from 'react';
 import './HealthyRecipes.css';
 import Header from './Header';
+import chicken from '../Pictures/chicken.jpg';
+import pizza from '../Pictures/pizza.jpg';
+import spaghetti from '../Pictures/spagheti.jpg';
 class HealthyRecipes extends Component {
     render() {
         return (
             <div>
-            <Header/>
-                HEALTHY RECIPES
+                <Header />
+                <div className="healthy-recipes">
+                    <div className="media-title" flex-direction="row">
+                        <h3 className="media-title-h3"> All Recipes</h3>
+                        <div>
+                            <div className="card">
+                                <div className="row">
+                                    <a className="col">
+                                        <img className="card-img-top" src={chicken} alt="Generic placeholder image" />
+                                        <div className="card-body">
+                                            <h5 className="card-title" align="center">Chicken Dry</h5>
+                                        </div>
+                                    </a>
+                                    <a className="col">
+                                        <img className="card-img-top" src={pizza} alt="Generic placeholder image" />
+                                        <div className="card-body">
+                                            <h5 className="card-title" align="center">Pizza</h5>
+                                        </div>
+                                    </a>
+                                    <a className="col">
+                                        <img className="card-img-top" src={spaghetti} alt="Generic placeholder image" />
+                                        <div className="card-body">
+                                            <h5 className="card-title" align="center">Spaghetti</h5>
+                                        </div>
+                                    </a>
+                                    <a className="col">
+                                        <img className="card-img-top" src={pizza} alt="Generic placeholder image" />
+                                        <div className="card-body">
+                                            <h5 className="card-title" align="center">Pizza</h5>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div className="card">
+                                <div className="row">
+                                    <a className="col">
+                                        <img className="card-img-top" src={spaghetti} alt="Generic placeholder image" />
+                                        <div className="card-body">
+                                            <h5 className="card-title" align="center">Spaghetti</h5>
+                                        </div>
+                                    </a>
+                                    <a className="col">
+                                        <img className="card-img-top" src={chicken} alt="Generic placeholder image" />
+                                        <div className="card-body">
+                                            <h5 className="card-title" align="center">Chicken Dry</h5>
+                                        </div>
+                                    </a>
+                                    <a className="col">
+                                        <img className="card-img-top" src={pizza} alt="Generic placeholder image" />
+                                        <div className="card-body">
+                                            <h5 className="card-title" align="center">Pizza</h5>
+                                        </div>
+                                    </a>
+                                    <a className="col">
+                                        <img className="card-img-top" src={spaghetti} alt="Generic placeholder image" />
+                                        <div className="card-body">
+                                            <h5 className="card-title" align="center">Spaghetti</h5>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div align="center" backgroundColor="red">
+                                <button> Show More</button>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
             </div>
         );
     }
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,16 +2,41 @@ import React, { Component } from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import maintiitle from '../Pictures/maintittle.png';
-import chicken from '../Pictures/ship3.jpg';
+import chicken from '../Pictures/chicken.jpg';
+import pizza from '../Pictures/pizza.jpg';
+import spaghetti from '../Pictures/spagheti.jpg';
 import './Main.css';
 class Main extends Component {
-    render() {
-        return (
-            <div>
-                <Header />
-                <div className="Main">
-                    <div className="media">
-                        <div className="media-body">
+        render() {
+    return (
+        <div>
+            <Header />
+            <div className="Main">
+                <div className="media">
+                    {/* <div className="slideshow-container">
+                        <div className="mySlides fade">
+                            <img src={maintiitle} />
+                        </div>
+
+                        <div className="mySlides fade">
+                            <img src={pizza} />
+                        </div>
+
+                        <div className="mySlides fade">
+                            <img src={maintiitle} />
+                        </div>
+
+                        <a className="prev" onClick="plusSlides(-1)">&#10094;</a>
+                        <a className="next" onClick="plusSlides(1)">&#10095;</a>
+
+                    </div>
+                    <br />
+                    <div style="text-align:center">
+                        <span className="dot" onClick="currentSlide(1)"></span>
+                        <span className="dot" onClick="currentSlide(2)"></span>
+                        <span className="dot" onClick="currentSlide(3)"></span>
+                    </div> */}
+                    <div className="media-body">
                             <a>
                                 <p>
                                     Who Are Little Chef ?
@@ -25,78 +50,78 @@ class Main extends Component {
                             </a>
                         </div>
                         <img className="mr-3" src={maintiitle} alt="Generic placeholder image" />
-                    </div>
-                    <br />
-                    <div className="media-title" flex-direction="row">
-                        <h3 className="media-title-h3"> All Recipes</h3>
-                        <div>
-                            <div className="card">
-                                <div className="row">
-                                    <a className="col">
-                                        <img className="card-img-top" src={chicken} alt="Generic placeholder image" />
-                                        <div className="card-body">
-                                            <h5 className="card-title" align="center">Chicken Dry</h5>
-                                        </div>
-                                    </a>
-                                    <a className="col">
-                                        <img className="card-img-top" src={chicken} alt="Generic placeholder image" />
-                                        <div className="card-body">
-                                            <h5 className="card-title" align="center">Chicken Dry</h5>
-                                        </div>
-                                    </a>
-                                    <a className="col">
-                                        <img className="card-img-top" src={chicken} alt="Generic placeholder image" />
-                                        <div className="card-body">
-                                            <h5 className="card-title" align="center">Chicken Dry</h5>
-                                        </div>
-                                    </a>
-                                    <a className="col">
-                                        <img className="card-img-top" src={chicken} alt="Generic placeholder image" />
-                                        <div className="card-body">
-                                            <h5 className="card-title" align="center">Chicken Dry</h5>
-                                        </div>
-                                    </a>
-                                </div>
+                </div>
+                <br />
+                <div className="media-title" flex-direction="row">
+                    <h3 className="media-title-h3"> All Recipes</h3>
+                    <div>
+                        <div className="card">
+                            <div className="row">
+                                <a className="col">
+                                    <img className="card-img-top" src={chicken} alt="Generic placeholder image" />
+                                    <div className="card-body">
+                                        <h5 className="card-title" align="center">Chicken Dry</h5>
+                                    </div>
+                                </a>
+                                <a className="col">
+                                    <img className="card-img-top" src={pizza} alt="Generic placeholder image" />
+                                    <div className="card-body">
+                                        <h5 className="card-title" align="center">Pizza</h5>
+                                    </div>
+                                </a>
+                                <a className="col">
+                                    <img className="card-img-top" src={spaghetti} alt="Generic placeholder image" />
+                                    <div className="card-body">
+                                        <h5 className="card-title" align="center">Spaghetti</h5>
+                                    </div>
+                                </a>
+                                <a className="col">
+                                    <img className="card-img-top" src={pizza} alt="Generic placeholder image" />
+                                    <div className="card-body">
+                                        <h5 className="card-title" align="center">Pizza</h5>
+                                    </div>
+                                </a>
                             </div>
-                            <div className="card">
-                                <div className="row">
-                                    <a className="col">
-                                        <img className="card-img-top" src={chicken} alt="Generic placeholder image" />
-                                        <div className="card-body">
-                                            <h5 className="card-title" align="center">Chicken Dry</h5>
-                                        </div>
-                                    </a>
-                                    <a className="col">
-                                        <img className="card-img-top" src={chicken} alt="Generic placeholder image" />
-                                        <div className="card-body">
-                                            <h5 className="card-title" align="center">Chicken Dry</h5>
-                                        </div>
-                                    </a>
-                                    <a className="col">
-                                        <img className="card-img-top" src={chicken} alt="Generic placeholder image" />
-                                        <div className="card-body">
-                                            <h5 className="card-title" align="center">Chicken Dry</h5>
-                                        </div>
-                                    </a>
-                                    <a className="col">
-                                        <img className="card-img-top" src={chicken} alt="Generic placeholder image" />
-                                        <div className="card-body">
-                                            <h5 className="card-title" align="center">Chicken Dry</h5>
-                                        </div>
-                                    </a>
-                                </div>
+                        </div>
+                        <div className="card">
+                            <div className="row">
+                                <a className="col">
+                                    <img className="card-img-top" src={spaghetti} alt="Generic placeholder image" />
+                                    <div className="card-body">
+                                        <h5 className="card-title" align="center">Spaghetti</h5>
+                                    </div>
+                                </a>
+                                <a className="col">
+                                    <img className="card-img-top" src={chicken} alt="Generic placeholder image" />
+                                    <div className="card-body">
+                                        <h5 className="card-title" align="center">Chicken Dry</h5>
+                                    </div>
+                                </a>
+                                <a className="col">
+                                    <img className="card-img-top" src={pizza} alt="Generic placeholder image" />
+                                    <div className="card-body">
+                                        <h5 className="card-title" align="center">Pizza</h5>
+                                    </div>
+                                </a>
+                                <a className="col">
+                                    <img className="card-img-top" src={spaghetti} alt="Generic placeholder image" />
+                                    <div className="card-body">
+                                        <h5 className="card-title" align="center">Spaghetti</h5>
+                                    </div>
+                                </a>
                             </div>
-                            <div align="center" backgroundColor="red">
-                                <button> Show More</button>
-                            </div>
+                        </div>
+                        <div align="center" backgroundColor="red">
+                            <button> Show More</button>
                         </div>
                     </div>
                 </div>
-                <Footer/>
             </div>
+            <Footer />
+        </div>
 
-        );
-    }
+    );
+}
 }
 
 export default Main;
