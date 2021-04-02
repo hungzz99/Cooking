052a06afcefafd0ffcbf112fcc0b17e93ceedd84
kongacute <binhttgch17201@fpt.edu.vn>
2021-04-02 16:39:14 +0700

    Added Firebase and FirebaseUI

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -3,6 +3,7 @@ import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
 import './Login.css';
 import { MDBIcon, MDBBtn } from "mdbreact";
+import SignInScreen from '../components/SignInScreen/SignInScreen';
 class Login extends Component {
     render() {
         return (
@@ -11,33 +12,7 @@ class Login extends Component {
                 <div className="login">
                     <div className="box-sizing1" >
                         <br/>
-                        <MDBIcon icon="user-circle" size="3x"/>
-                                                                        <form className="px-4 py-3">
-                            <div className="form-group">
-                                <input type="user" className="form-control" placeholder="Username -or- Email Address" />
-                            </div>
-                            <div className="form-group">
-                                <input type="password" className="form-control" id="exampleDropdownFormPassword1" placeholder="Password" />
-                            </div>
-                            <div className="form-check">
-                                <input type="checkbox" className="form-check-input" id="dropdownCheck" />
-                                <label className="form-check-label" for="dropdownCheck">
-                                    Remember me
-                                </label>
-                            </div>
-                            <br />
-                            <button type="submit" className="btn btn-primary" >Sign in</button>
-                        </form>
-                        <div className="a" >
-                            <a href="/sign-up">If you don't have account, Sign up here!</a>
-                            <br />
-                            <a href="#">Forgot password?</a>
-                        </div>
-                        <form className="px-4 py-3" action="/login">
-                            <MDBBtn social="gplus">
-                                <MDBIcon fab icon="google-plus-g" className="pr-1" /> Login With Google +
-                            </MDBBtn>
-                        </form>
+                        <SignInScreen />
                     </div>
                 </div>
                 <Footer/>
--- src/index.js
+++ src/index.js
@@ -4,7 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Router } from "react-router";
+import firebase from "firebase";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDcv40AZwEIt7rcOeUk6kxU_mNnbIzE_J4",
+  authDomain: "cooking-forum.firebaseapp.com",
+  databaseURL: "https://cooking-forum-default-rtdb.firebaseio.com",
+  projectId: "cooking-forum",
+  storageBucket: "cooking-forum.appspot.com",
+  messagingSenderId: "188904882071",
+  appId: "1:188904882071:web:3a166e2b57ab3544d00ed2",
+  measurementId: "G-MSPX7YQE2H"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
